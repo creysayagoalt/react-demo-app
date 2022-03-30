44ec924e8c60f9e71d1584a6e58819a0a122efd4
crey <crey@altimetrik.com>
2022-03-30 11:29:42 -0300

    this should not be deployed

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,6 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("how many people likes this talk", () => {
-  const likesNumber = 9;
+  const likesNumber = 5;
   expect(likesNumber).toBeGreaterThanOrEqual(8);
 });
