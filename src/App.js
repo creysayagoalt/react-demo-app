fa2415b2207f7c1a543403513ae7efdeea42cea7
crey <crey@altimetrik.com>
2022-03-30 10:03:12 -0300

    pass

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This deploy will pass
         </a>
       </header>
     </div>
