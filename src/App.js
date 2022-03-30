7e04b59bc98c3b8120579146a5fd6c7bba6773be
crey <crey@altimetrik.com>
2022-03-30 10:05:17 -0300

    fails

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          This deploy will pass
+          This deploy will not pass
         </a>
       </header>
     </div>
